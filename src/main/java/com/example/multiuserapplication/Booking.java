f278f2be78621444558505f13e78d0c29e0e3107
hereIsLucas <lucas.heroin@lernende.bbw.ch>
2024-07-10 16:14:58 +0200

    initialize: booking

--- src/main/java/com/example/multiuserapplication/Booking.java
+++ src/main/java/com/example/multiuserapplication/Booking.java
@@ -1,9 +1,50 @@
 package com.example.multiuserapplication;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+
+import java.util.Date;
+
 /**
  * @author : lucas
  * @project : MultiUserApplication
  * @created : 10/07/2024, Wednesday
  **/
+@Getter
 public class Booking {
+    // Getter und Setter
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private Date date;
+    private String day;
+
+    @ManyToOne
+    private Room room;
+
+    @ManyToOne
+    private User user;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public void setDay(String day) {
+        this.day = day;
+    }
+
+    public void setRoom(Room room) {
+        this.room = room;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
