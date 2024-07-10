d7242b08b70bcdb2c782dc3fc4eb3ca6f4f22d37
hereIsLucas <lucas.heroin@lernende.bbw.ch>
2024-07-10 15:49:47 +0200

    initial commit

---
+++ src/main/java/com/example/multiuserapplication/MultiUserApplication.java
@@ -0,0 +1,13 @@
+package com.example.multiuserapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultiUserApplication.class, args);
+    }
+
+}
