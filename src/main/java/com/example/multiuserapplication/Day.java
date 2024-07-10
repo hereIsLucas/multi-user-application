c834f306a79ec2b97b1309b61fb08eee19625cff
hereIsLucas <lucas.heroin@lernende.bbw.ch>
2024-07-10 16:16:55 +0200

    initialize: days

--- src/main/java/com/example/multiuserapplication/Day.java
+++ src/main/java/com/example/multiuserapplication/Day.java
@@ -1,9 +1,20 @@
 package com.example.multiuserapplication;
 
+import jakarta.persistence.Id;
+import lombok.Getter;
+
 /**
  * @author : lucas
  * @project : MultiUserApplication
  * @created : 10/07/2024, Wednesday
  **/
+@Getter
 public class Day {
+    // Getter und Setter
+    @Id
+    private String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
