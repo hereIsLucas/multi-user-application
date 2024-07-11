29a32e6c7795ff5a1d5852f5ea1a81e6dda84131
hereIsLucas <lucas.heroin@lernende.bbw.ch>
2024-07-11 09:20:46 +0200

    instantiate: DayREpository

--- src/main/java/com/example/multiuserapplication/domain/Day.java
+++ src/main/java/com/example/multiuserapplication/domain/Day.java
@@ -1,30 +1,26 @@
 package com.example.multiuserapplication.domain;
 
+
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
-import lombok.Getter;
+import lombok.Data;
 
 import java.util.List;
-
 /**
  * @author : lucas
  * @project : MultiUserApplication
  * @created : 10/07/2024, Wednesday
  **/
-@Getter
+@Data
 public class Day {
-    // Getter und Setter
     @Id
     private String name;
 
     @OneToMany(mappedBy = "day")
     private List<Booking> bookings;
 
-    public void setName(String name) {
+    public Day(String name, List<Booking> bookings) {
         this.name = name;
-    }
-
-    public void setBookings(List<Booking> bookings) {
         this.bookings = bookings;
     }
 }
---
+++ src/main/java/com/example/multiuserapplication/repositories/DayRepository.java
@@ -0,0 +1,12 @@
+package com.example.multiuserapplication.repositories;
+
+import com.example.multiuserapplication.domain.Day;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * @author : lucas
+ * @project : MultiUserApplication
+ * @created : 11/07/2024, Thursday
+ **/
+public interface DayRepository extends JpaRepository<Day, Long> {
+}
